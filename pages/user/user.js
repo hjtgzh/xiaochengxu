2a5a122fa339fc9d00fb6550fc131e370f7e0ba6
hjtgzh1 <huangjianting@zhiweicloud.com>
2017-12-20 15:11:22 +0800

    小程序初步完成

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,37 +1,60 @@
 Page({
+  onLoad: function () {
+    this.setData({
+      date: (new Date()).toLocaleDateString().split('/').join('-'),
+      time: (new Date()).toTimeString().split(' ')[0].slice(0, 5),
+    })
+  },
   data: {
-    radioItems: [
-      { name: 'cell standard', value: '0' },
-      { name: 'cell standard', value: '1', checked: true }
-    ],
-    checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: true },
-      { name: 'standard is dealicient for u.', value: '1' }
-    ],
+    phoneNumber: '',
+    verificationCode: '',
+    date: '',
+    time: '',
 
-    date: "2016-09-01",
-    time: "12:01",
-
-    countryCodes: ["+86", "+80", "+84", "+87"],
-    countryCodeIndex: 0,
-
-    countries: ["中国", "美国", "英国"],
-    countryIndex: 0,
+    phoneTypeCodes: ["+86", "+80", "+84", "+87"],
+    phoneTypeIndex: 0,
+    phoneType: '+86',
 
     accounts: ["微信号", "QQ", "Email"],
     accountIndex: 0,
+    account: '微信号',
+
+    countries: ["中国", "美国", "英国"],
+    countryIndex: 0,
+    country: '中国',
+
     isAgree: false
   },
-  showTopTips: function () {
-    var that = this;
+  bindPhoneTypeChange: function (e) {
     this.setData({
-      showTopTips: true
-    });
-    setTimeout(function () {
-      that.setData({
-        showTopTips: false
-      });
-    }, 3000);
+      phoneTypeIndex: e.detail.value,
+      phoneType: phoneTypeCodes[e.detail.value]
+    })
+  },
+  phoneNumberChange: function (e) {
+    this.setData({
+      phoneNumber: e.detail.value
+    })
+  },
+  getVerificationCode: function () {
+    console.log('获取验证码')
+    wx.request({
+      url: 'http://rap.taobao.org/mockjsdata/26789/home/list', //仅为示例，并非真实的接口地址
+      data: { },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      method: 'POST',
+      success: function (res) {
+        console.log(res)
+        console.log(res.data)
+      }
+    })
+  },
+  verificationCodeChange: function (e) {
+    this.setData({
+      verificationCode: e.detail.value
+    })
   },
   bindDateChange: function (e) {
     this.setData({
@@ -43,30 +66,32 @@ Page({
       time: e.detail.value
     })
   },
-  bindCountryCodeChange: function (e) {
-    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
-
+  bindAccountChange: function (e) {
     this.setData({
-      countryCodeIndex: e.detail.value
+      accountIndex: e.detail.value,
+      account: accounts[e.detail.value]
     })
   },
   bindCountryChange: function (e) {
-    console.log('picker country 发生选择改变，携带值为', e.detail.value);
-
     this.setData({
-      countryIndex: e.detail.value
-    })
-  },
-  bindAccountChange: function (e) {
-    console.log('picker account 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      accountIndex: e.detail.value
+      countryIndex: e.detail.value,
+      country: countries[e.detail.value]
     })
   },
   bindAgreeChange: function (e) {
     this.setData({
       isAgree: !!e.detail.value.length
     });
-  }
+  },
+  handleLoad: function () {
+    const { phoneType, phoneNumber, verificationCode, date, time, account, country, isAgree } = this.data
+    console.log(phoneType)
+    console.log(phoneNumber)
+    console.log(verificationCode)
+    console.log(date)
+    console.log(time)
+    console.log(account)
+    console.log(country)
+    console.log(isAgree)
+  },
 })
